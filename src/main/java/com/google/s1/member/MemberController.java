b2ff9d7077d7a625443893f6d660d7bddb7ca658
gjrmsp <gjrmsp@naver.com>
2021-03-19 09:39:45 +0900

    update

--- src/main/java/com/google/s1/member/MemberController.java
+++ src/main/java/com/google/s1/member/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -42,7 +43,7 @@ public class MemberController {
 
 	//memberLogin2 print
 	@RequestMapping(value = "/member/memberLogin", method = RequestMethod.POST)
-	public void memberLogin2(HttpServletRequest request) throws Exception {
+	public String memberLogin(HttpServletRequest request) throws Exception {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 
@@ -52,5 +53,7 @@ public class MemberController {
 		memberDTO = memberService.memberLogin(memberDTO);
 
 		System.out.println(memberDTO);
+		request.setAttribute("dto", memberDTO);
+		return "member/memberPage";
 	}
 }
