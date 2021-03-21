c99334357a98ed92f71117139580e642a15c60e4
gjrmsp <gjrmsp@naver.com>
2021-03-22 01:49:33 +0900

    Update MemberController.java

--- src/main/java/com/google/s1/member/MemberController.java
+++ src/main/java/com/google/s1/member/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
@@ -31,7 +32,7 @@ public class MemberController {
 
 	//memberJoin print	//	/member/memberLogin
 	@RequestMapping(value = "/member/memberLogin")
-	public String memberLogin(String name, int age) {
+	public String memberLogin() {
 		//		String name = request.getParameter("name");
 		//		int age = Integer.parseInt(request.getParameter("age"));
 		//		System.out.println(name);
@@ -43,17 +44,13 @@ public class MemberController {
 
 	//memberLogin2 print
 	@RequestMapping(value = "/member/memberLogin", method = RequestMethod.POST)
-	public String memberLogin(HttpServletRequest request) throws Exception {
-		String id = request.getParameter("id");
-		String pw = request.getParameter("pw");
-
-		MemberDTO memberDTO = new MemberDTO();
-		memberDTO.setId(id);
-		memberDTO.setPw(pw);		
+	public ModelAndView memberLogin(MemberDTO memberDTO) throws Exception {
+		ModelAndView modelAndView = new ModelAndView();
 		memberDTO = memberService.memberLogin(memberDTO);
-
-		System.out.println(memberDTO);
-		request.setAttribute("dto", memberDTO);
-		return "member/memberPage";
+		
+		modelAndView.addObject("dto", memberDTO);
+		modelAndView.setViewName("member/memberPage");
+		
+		return modelAndView;
 	}
 }
